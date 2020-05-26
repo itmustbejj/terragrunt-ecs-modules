3aef628f350d4890e5628ebfec4576695caf3a05
Josh Hudson <jjhudson1@gmail.com>
2020-05-25 21:07:15 -0600

    Terraform fmt

--- ecs/main.tf
+++ ecs/main.tf
@@ -1,10 +1,10 @@
 module "ecs_cluster" {
   source = "git@github.com:itmustbejj/terraform-aws-ecs//modules/cluster"
 
-  asg_min_size    = var.asg_min_size
-  asg_max_size    = var.asg_max_size
-  name        = var.name
-  vpc_id      = var.vpc_id
-  vpc_subnets = var.vpc_subnets
-  tags        = var.tags
+  asg_min_size = var.asg_min_size
+  asg_max_size = var.asg_max_size
+  name         = var.name
+  vpc_id       = var.vpc_id
+  vpc_subnets  = var.vpc_subnets
+  tags         = var.tags
 }
